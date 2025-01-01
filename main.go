30cc6e3fd36dc7fa1bd3e289d22550ddde3fe4ab
Nauval Atmaja <nauval.atmaja@gmail.com>
2025-01-01 09:58:32 +0700

    feat: 3LI-15 options /file endpoint
    https://linear.app/3lightyears/issue/3LI-15/options-file

--- main.go
+++ main.go
@@ -7,12 +7,28 @@ import (
 	"encoding/json"
 	"log/slog"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
 
 	"github.com/google/uuid"
 )
 
+const (
+	MAX_SIZE             int64 = 1024 * 1024 * 1024
+	TUS_PROTOCOL_VERSION       = "1.0.0"
+
+	//	headers
+	HEADER_TUS_RESUMABLE = "Tus-Resumable"
+	HEADER_TUS_VERSION   = "Tus-Version"
+	HEADER_TUS_EXTENSION = "Tus-Extension"
+	HEADER_TUS_MAX_SIZE  = "Tus-Max-Size"
+)
+
 func main() {
-	mux := buildServeMux()
+	mux := buildServeMux(&ServerConfig{
+		UploadDir: "upload",
+	})
 
 	// starting the app
 	slog.Info("running app at :1080")
@@ -25,7 +41,11 @@ type FileInitResponse struct {
 	ID string `json:"id"`
 }
 
-func buildServeMux() *http.ServeMux {
+type ServerConfig struct {
+	UploadDir string // the directory wher all file is being uploaded to
+}
+
+func buildServeMux(config *ServerConfig) *http.ServeMux {
 	mux := http.NewServeMux()
 	// POST /file => create session
 	mux.HandleFunc("POST /file", func(w http.ResponseWriter, r *http.Request) {
@@ -35,6 +55,14 @@ func buildServeMux() *http.ServeMux {
 			http.Error(w, "Error allocating id", http.StatusInternalServerError)
 			return
 		}
+
+		dir := filepath.Join(config.UploadDir, id.String())
+		if err = os.MkdirAll(dir, 0666); err != nil {
+			slog.Error("Failed to create file directory", slog.String("dir", dir), slog.Any("Error", err))
+			http.Error(w, "Error allocating storage", http.StatusInternalServerError)
+			return
+		}
+
 		resp := FileInitResponse{ID: id.String()}
 		jsonResponse, err := json.Marshal(resp)
 		if err != nil {
@@ -45,6 +73,15 @@ func buildServeMux() *http.ServeMux {
 		w.Header().Add("Content-Type", "application/json")
 		w.WriteHeader(http.StatusCreated)
 		w.Write(jsonResponse)
+	})
+
+	// Options
+	mux.HandleFunc("OPTIONS /file", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set(HEADER_TUS_RESUMABLE, TUS_PROTOCOL_VERSION)
+		w.Header().Set(HEADER_TUS_VERSION, TUS_PROTOCOL_VERSION)
+		w.Header().Add(HEADER_TUS_EXTENSION, "creation")
+		w.Header().Set(HEADER_TUS_MAX_SIZE, strconv.Itoa(int(MAX_SIZE)))
+		w.WriteHeader(http.StatusNoContent)
 	})
 
 	// Head => show status
--- main_test.go
+++ main_test.go
@@ -5,33 +5,23 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
 	"sync"
 	"testing"
 
 	"github.com/google/uuid"
 )
 
-var serverAddr string
+var serverAddr, uploadDir string
 
 func TestMain(m *testing.M) {
-	// addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
-	// if err != nil {
-	// 	slog.Error("Failed to get available port for testing", slog.Any("Error", err))
-	// 	os.Exit(1)
-	// }
-	//
-	// listener, err := net.ListenTCP("tcp", addr)
-	// if err != nil {
-	// 	slog.Error("Failed to get available port for testing", slog.Any("Error", err))
-	// 	os.Exit(1)
-	// }
-	//
-	// defer listener.Close()
-	// serverAddr = listener.Addr().(*net.TCPAddr).String()
 	serverAddr = "localhost:1071"
+	uploadDir = os.TempDir()
 
 	// run server
-	mux := buildServeMux()
+	mux := buildServeMux(&ServerConfig{
+		UploadDir: uploadDir,
+	})
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
@@ -40,6 +30,10 @@ func TestMain(m *testing.M) {
 	}()
 
 	exit := m.Run()
+
+	// clean up
+	os.RemoveAll(uploadDir)
+
 	os.Exit(exit)
 }
 
@@ -74,5 +68,59 @@ func TestFileShouldReturn201WithNewFileId(t *testing.T) {
 	_, err = uuid.Parse(r.ID)
 	if err != nil {
 		t.Errorf("POST /file does not return correct uuid. got=%v", err)
+	}
+
+	// create a temp directory
+	dir := filepath.Join(uploadDir, r.ID)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		t.Errorf("POST /file does not create the %s dir. got=%v", r.ID, err)
+	}
+}
+
+func TestOptionsShouldReturn204(t *testing.T) {
+	tests := []struct {
+		host                    string
+		headers                 map[string]string
+		expectedResponseStatus  int
+		expectedResponseHeaders map[string]string
+	}{
+		{
+			host: fmt.Sprintf("http://%s", serverAddr),
+			headers: map[string]string{
+				"Host": serverAddr,
+			},
+			expectedResponseStatus: http.StatusNoContent,
+			expectedResponseHeaders: map[string]string{
+				"Tus-Resumable": "1.0.0",
+				"Tus-Version":   "1.0.0",
+				"Tus-Max-Size":  "1073741824", // 1GB
+				"Tus-Extension": "creation",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		req, err := http.NewRequest(http.MethodOptions, fmt.Sprintf("%s/file", tt.host), nil)
+		for k, v := range tt.headers {
+			req.Header.Add(k, v)
+		}
+		if err != nil {
+			t.Fatalf("Fail to create new request: %v", err)
+		}
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			t.Fatalf("Fail to execute the request. err=%v", err)
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode != tt.expectedResponseStatus {
+			t.Errorf("OPTIONS /file does not return %v. got=%v", tt.expectedResponseStatus, res.StatusCode)
+		}
+
+		for k, v := range tt.expectedResponseHeaders {
+			if res.Header.Get(k) != v {
+				t.Errorf("OPTIONS /file does not return correct value for header %v, expected=%v. got=%v", k, v, res.Header.Get(k))
+			}
+		}
 	}
 }
